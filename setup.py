1048d8e99ab0a8a0890ab2d7df56baecfa1ce7d8
Eytan Balken <eytan.balken@mail.huji.ac.il>
2024-02-04 12:02:58 +0200

    Version with python 3.12

--- setup.py
+++ setup.py
@@ -159,7 +159,7 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
-        #'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.12',
     ],
     cmdclass={
         'prepare': PrepareCommand,
